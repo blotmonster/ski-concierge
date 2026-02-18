aa49c16d9bc2e68713db2c9df7cadf37dec952af
blotmonster <jonblotner@gmail.com>
2026-02-18 17:29:38 -0500

    Create resorts.js

---
+++ resorts.js
@@ -0,0 +1,20 @@
+const resorts = [
+
+  { name:"Stowe", lat:44.53, lon:-72.78, vertical:8, expert:9, groomers:7, snow:7, luxury:8, nightlife:6, tier:8 },
+  { name:"Sugarbush", lat:44.14, lon:-72.89, vertical:8, expert:8, groomers:6, snow:7, luxury:6, nightlife:5, tier:7 },
+  { name:"Killington", lat:43.60, lon:-72.82, vertical:7, expert:7, groomers:8, snow:7, luxury:6, nightlife:8, tier:7 },
+  { name:"Mount Sunapee", lat:43.38, lon:-72.06, vertical:5, expert:4, groomers:8, snow:5, luxury:5, nightlife:3, tier:5 },
+  { name:"Cannon", lat:44.16, lon:-71.70, vertical:7, expert:8, groomers:5, snow:6, luxury:3, nightlife:2, tier:6 },
+  { name:"Sugarloaf", lat:45.03, lon:-70.31, vertical:9, expert:9, groomers:6, snow:8, luxury:5, nightlife:4, tier:8 },
+  { name:"Sunday River", lat:44.47, lon:-70.85, vertical:7, expert:6, groomers:8, snow:7, luxury:6, nightlife:5, tier:7 },
+
+  { name:"Jackson Hole", lat:43.59, lon:-110.83, vertical:10, expert:10, groomers:5, snow:9, luxury:7, nightlife:6, tier:10 },
+  { name:"Alta", lat:40.59, lon:-111.64, vertical:9, expert:9, groomers:4, snow:10, luxury:4, nightlife:2, tier:9 },
+  { name:"Snowbird", lat:40.58, lon:-111.66, vertical:9, expert:9, groomers:5, snow:9, luxury:6, nightlife:4, tier:9 },
+  { name:"Deer Valley", lat:40.62, lon:-111.48, vertical:8, expert:6, groomers:9, snow:8, luxury:10, nightlife:6, tier:9 },
+  { name:"Park City", lat:40.65, lon:-111.51, vertical:8, expert:7, groomers:8, snow:7, luxury:8, nightlife:8, tier:8 },
+
+  { name:"Vail", lat:39.64, lon:-106.37, vertical:9, expert:8, groomers:9, snow:8, luxury:9, nightlife:8, tier:10 },
+  { name:"Telluride", lat:37.94, lon:-107.84, vertical:9, expert:9, groomers:6, snow:8, luxury:9, nightlife:6, tier:9 }
+
+];
